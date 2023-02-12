aa9ecdf0574d4c0ad41bc14c5e7dc80eefb27289
Juan Esteban Bravo <juanestebanba@gmail.com>
2023-02-12 19:40:40 +0000

    Finished individual images size and display

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useNavigate, Link, useParams } from 'react-router-dom'; 
+import imagelist from '../images.json';
+import '../styles/CardDetail.scss';
 
 const CardDetail = () => {
+  const navigate = useNavigate();
+  const { slug } = useParams();
+
+  const individualImage = imagelist.find(image => image.slug === slug);
+
+//OJO; PASAR IMAGENES QQL DE PNG A WEBP
 	return (
-    <h1> Cards detail</h1>
+    <div className="individual-card-detail">
+      <h3 className="individual-image-title">{individualImage.name}</h3>
+      
+      <div className="individual-card-flex-container">
+      
+
+      <img className="individual-image" src={individualImage.path} alt="image" />
+      </div>
+    </div>
 	);
 }
 
